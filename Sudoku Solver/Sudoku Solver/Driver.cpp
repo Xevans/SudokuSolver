ddfece7c9c07639f8dc8c93b29fe9b5635f6aae2
xavier evans <evans.xavier@ymail.com>
2023-08-17 16:14:41 -0400

    generate function added and tested.
    See notes.
    Pushed after finishing integration testing. Algorithm modification needed.

--- Sudoku Solver/Sudoku Solver/Driver.cpp
+++ Sudoku Solver/Sudoku Solver/Driver.cpp
@@ -10,7 +10,27 @@ int main() {
 
 	Sudoku game;
 
-	bool test_validate = game.validate();
+	//bool test_validate = game.validate();
+	//cout << "Status of validate: " << test_validate << endl;
 
-	cout << "Status of validate: " << test_validate << endl;
+	game.generate();
+	game.printBoard();
+
+	/*for (int i = 0; i < 80; i++) {
+		game.generate();
+		game.printBoard();
+	}*/
+
+	while (game.inserted_items != MAX) {
+		game.generate();
+		game.printBoard();
+	}
+
+	//game.board[8][7]->value = 1;
+	//game.board[8][8]->value = 2;
+	//game.printBoard();
+	//game.validate(7, 6, 2);
+	
+
+	// similate outer while loop by putting a for loop here 
 }
--- Sudoku Solver/Sudoku Solver/Sudoku.h
+++ Sudoku Solver/Sudoku Solver/Sudoku.h
@@ -1,7 +1,7 @@
 // generates a sudoku board and pre fills extries in each sector
 #include <iostream>
 #include <vector>
-#include <ctime>
+#include <random>
 using namespace std;
 
 #define MAX 81 // total Cells that exist on a 9x9 Sudoku board
@@ -13,6 +13,7 @@ struct cell {
 	int cell_number = 0;
 	int value = 0;
 	int sector = 0;
+	bool status = false;
 	//cell* next_cell;
 };
 
@@ -27,13 +28,15 @@ public:
 	//vector<cell*> board;
 	int reference_points[9]{11, 14, 17, 38, 41, 44, 65, 68, 71}; // list of every subsection's central identifier
 
+	vector<int> occupied_cells;
+
 	// Opertations
 	Sudoku();
 	~Sudoku();
 
 	void initialize(); // initialize a list of 9 2D matricies with each cell's values initialized.
 	void generate(); // Randomly choose a cell and insert a single digit.
-	bool validate(); // verify that the board follows the rules of sudoku. Two step process.
+	bool validate(int, int, int); // verify that the board follows the rules of sudoku. Two step process.
 	bool subset_validate(int, int, int); // checks if the current entry already exists in that sector.
 	bool cross_validate(int, int, int); // Checks if the current entry already exists in the row and column it was placed in.
 	
@@ -71,16 +74,18 @@ void Sudoku::initialize() {
 			cell* temp = new cell;
 			temp->cell_number = counter;
 			temp->value = 0;
+			temp->status = false;
 			board[i][j] = temp;
 			counter++;
 		}
 	}
-
+/*
 	for (int i = 0; i < grid; i++) {
 		for (int j = 0; j < grid; j++) {
 			cout << board[i][j]->value << endl;
 		}
 	}
+	*/
 
 	// initialize reference points
 
@@ -93,26 +98,87 @@ void Sudoku::initialize() {
 
 
 void Sudoku::generate() {
+	
+	bool existing = false;
+	bool valid = false;
+	int row = -1;
+	int col = -1;
+	int entry = -1;
+
+	random_device rd;
+	mt19937 gen(rd());
+	uniform_int_distribution<> dis(1, 81); // inclusive
+
+	// !!!(parent loop)!!! loop until inserted_items == 81 // remember to modify inserted items when debugging with existing values.
+
+	int random_number = dis(gen);
+
+	// if random number does not exist in array of occupied_cells (a list of cell numbers)
+	for (int i = 0; i < grid; i++) {
+		for (int j = 0; j < grid; j++) {
+			if (board[i][j]->cell_number == random_number) {
+				row = i;
+				col = j;
+				break;
+			}
+		}
+
+		if (row > -1 && col > -1) {
+			break;
+		}
+	}
+
+	// if chosen cell is not occupied, go to next step, otherwise loop
+	if (board[row][col]->status != true) {
+
+		// choose a number from the list in order until 1 or none satisfy
+		for (int i = 0; i < grid; i++) {
+
+			if (validate(row, col, i+1) == true) {
+				entry = i+1;
+				valid = true;
+				break;
+			}
+		}
+
+
+		//if entry is valid, write entry to cell coordinates.
+		if (valid) {
+
+			cout << "adding new entry: " << entry << " at " << row << "," << col << endl;
+
+			board[row][col]->value = entry;
+			board[row][col]->status = true;
+			inserted_items++;
+		}
+
+		
+	}
+	
+
+	// reset
+	existing = false;
+	row = -1;
+	col = -1;
+	entry = -1;
+
+	/*while (inserted_items < MAX) {
+		
+	}*/
 
 }
 
 
 
-bool Sudoku::validate() {
-	//Normally would take row and column numbers as arguments
-	int row = 2;
-	int column = 0;
-	int entry = 1;
+bool Sudoku::validate(int row, int col, int entry) {
 
-	board[1][1]->value = 3;
-
-
+	cout << "Validating... " << entry << " at " << row << "," << col << endl;
 
 	// call subset validation that returns true or false, store the value
-	bool subset_check = subset_validate(row, column, entry);
+	bool subset_check = subset_validate(row, col, entry);
 
 	// call cross validation that returns true or false, store the value
-	bool cross_check = cross_validate(row, column, entry);
+	bool cross_check = cross_validate(row, col, entry);
 
 	cout << "subset-check status: " << subset_check << endl;
 	cout << "cross-check status:" << cross_check << endl;
@@ -127,21 +193,54 @@ bool Sudoku::validate() {
 }
 
 void Sudoku::printBoard() {
-}
 
-bool Sudoku::subset_validate(int row, int col, int entry) {
+	cout << endl;
 
 	for (int i = 0; i < grid; i++) {
 		for (int j = 0; j < grid; j++) {
-			cout << board[i][j]->value << endl;
+			cout << board[i][j]->value << " ";
+
+			if ((j + 1) % 9 == 0) { // if the current col iteration is divisible by 9, end line
+				cout << endl;
+			} 
+			else if ((j + 1) % 3 == 0) { // if the current col iteration is not divisible by 9 but 3, add a bar
+				cout << "| ";
+			}
+		}
+
+		if ((i + 1) % 3 == 0) { // if the current row iteration is divisible by 3, display a divider
+			cout << "---------------------" << endl;
 		}
 	}
+
+	/*cout << "cell nums" << endl;
+
+	for (int i = 0; i < grid; i++) {
+		for (int j = 0; j < grid; j++) {
+			cout << board[i][j]->cell_number << " ";
+
+			if ((j + 1) % 9 == 0) { // if the current col iteration is divisible by 9, end line
+				cout << endl;
+			}
+			else if ((j + 1) % 3 == 0) { // if the current col iteration is not divisible by 9 but 3, add a bar
+				cout << "| ";
+			}
+		}
+
+		if ((i + 1) % 3 == 0) { // if the current row iteration is divisible by 3, display a divider
+			cout << "---------------------" << endl;
+		}
+	}*/
+}
+
+bool Sudoku::subset_validate(int row, int col, int entry) {
 
 	// determine center point of sector
 
 	// center point's coordinates
 	int reference_row = 0;
 	int reference_col = 0;
+	bool found = false; // When subset's center is found, mark true.
 
 	// box neighbor check for reference point coordinates
 	for (int i = row - 1; i <= row + 1; i++) {
@@ -160,14 +259,24 @@ bool Sudoku::subset_validate(int row, int col, int entry) {
 				if (board[i][j]->cell_number == reference_points[k]) {
 					reference_row = i;
 					reference_col = j;
+					found = true;
+					break;
 				}
 			}
+
+			if (found == true) {
+				break;
+			}
+		}
+
+		if (found == true) {
+			break;
 		}
 	}
 
 	// box neighbor check for identical entry to current arbitrary entry.
-	for (int i = reference_row - 1; i < reference_row + 1; i++) {
-		for (int j = reference_col - 1; j < reference_col + 1; j++) {
+	for (int i = reference_row - 1; i <= reference_row + 1; i++) {
+		for (int j = reference_col - 1; j <= reference_col + 1; j++) {
 
 			// if the value of the the current cell is the same as the entry AND the coordinates are NOT the same
 			if (board[i][j]->value == entry && i != row && j != col) {
